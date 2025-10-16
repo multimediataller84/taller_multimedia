c8cc1e17f88a40a233fc2b38657eb9095d311422
moichin <mmoraleschin5@gmail.com>
2025-10-16 17:30:56 -0600

    Prueba 4

--- src/modules/Dashboard/screen/Dashboard.tsx
+++ src/modules/Dashboard/screen/Dashboard.tsx
@@ -1,29 +1,11 @@
 import { RootLayout } from "../../../_Layouts/RootLayout";
 import IncomeCard from "../components/IncomeCard";
 import ProductsCard from "../components/ProductsPieChart"
-import { InvoiceHistoryTable } from "../../Invoices/components/InvoiceHistoryTable"
-import { useInvoiceHistoryDashboard } from "../hooks/useInvoiceHistoryDashboard";
-import { useState } from "react";
-import type { TInvoiceEndpoint } from "../../Invoices/models/types/TInvoiceEndpoint";
-import { InvoiceDetailModal } from "../../Invoices/components/InvoiceDetailModal";
 import Pagination from "../../../components/pagination";
+import { Reports } from "../../Reports/screen/Reports";
 
 export const Dashboard = () => {
 
-  const [selectedInvoice, setSelectedInvoice] = useState<TInvoiceEndpoint | null>(null);
-  const [detailOpen, setDetailOpen] = useState(false);
-
-   const {
-    currentInvoices,
-    totalPages,
-    activePage,
-    setActivePage,
-    pagesDisplay,
-    canPrev,
-    canNext,
-    goPrev,
-    goNext,
-  } = useInvoiceHistoryDashboard();
 
 
 const data = [
@@ -62,59 +44,9 @@ const data = [
 
         </div>
 
-        <div className="w-full px-8 mt-8 space-y-4">
-        <div className="flex w-full justify-between">
-          <h2 className="text-2xl font-Lato">Historial de Facturas más recientes</h2>
-          <div className="flex space-x-4">
+       
 
-            <div className="relative">
-              <select className="appearance-none w-[94px] border py-2 rounded-3xl px-4 text-gray1 border-gray2 bg-white font-medium text-base focus:outline-blue-500 focus:outline-2">
-                <option>24h</option>
-                <option>72h</option>
-              </select>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 absolute right-4 top-1/2 -translate-y-1/2 fill-gray1">
-                  <path fill-rule="evenodd" d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z" clip-rule="evenodd" />
-                </svg>
-              </div>
-
-              <div className="relative">
-              <select className="appearance-none w-[220px] border py-2 rounded-3xl px-4 text-gray1 border-gray2 bg-white font-medium text-base focus:outline-blue-500 focus:outline-2">
-                <option>De Menor a Mayor</option>
-                <option>De Mayor a Menor</option>
-              </select>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 absolute right-4 top-1/2 -translate-y-1/2 fill-gray1">
-                  <path fill-rule="evenodd" d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z" clip-rule="evenodd" />
-                </svg>
-              </div>
-            </div>
-        </div>
-
-        <InvoiceHistoryTable
-        data={currentInvoices}
-        onSelect={(inv) => {
-        setSelectedInvoice(inv);
-        setDetailOpen(true);
-        }}
-        />
-
-        <Pagination
-        totalPages={totalPages}
-        activePage={activePage}
-        setActivePage={setActivePage}
-        canPrev={canPrev}
-        canNext={canNext}
-        goPrev={goPrev}
-        goNext={goNext}
-        pagesDisplay={pagesDisplay}
-        />
-        </div>
-
-        
-         <InvoiceDetailModal
-            open={detailOpen}
-            invoice={selectedInvoice}
-            onClose={() => setDetailOpen(false)}
-          />
+          <Reports></Reports>
         
       </div>
     </RootLayout>
